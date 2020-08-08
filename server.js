30e76fc41107e8153872fa32db5d68b8fdefc591
Matheus Encarnação <matheus.encarnacao@hotmail.com>
2020-08-07 22:20:19 -0300

    Alterada variavel port para o heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middleware = jsonServer.defaults();
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 server.use(middleware);
 server.use(router);
